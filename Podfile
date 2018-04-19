6bb1a39c5b14f222cdbfbfd7b89cd1719d77cdf8
Dhiraj Das <dhiraj.das.05@gmail.com>
2018-04-20 00:50:14 +0530

    added realm

--- Podfile
+++ Podfile
@@ -7,6 +7,7 @@ target 'RxTweet' do
 pod 'RxSwift',    '~> 4.0'
 pod 'Alamofire', '~> 4.7'
 pod 'RxCocoa',    '~> 4.0'
+pod 'RealmSwift'
   # Pods for RxTweet
 
 end
---
+++ RxTweet/DataModels/Tweet.swift
@@ -0,0 +1,11 @@
+//
+//  Tweet.swift
+//  RxTweet
+//
+//  Created by Dhiraj Das on 4/19/18.
+//  Copyright © 2018 Dhiraj Das. All rights reserved.
+//
+
+import Foundation
+
+
